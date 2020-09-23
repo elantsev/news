033fec4e097c16c4487bf55a476cb040a67ac28b
elantsev <elantsev.pa@gmail.com>
2020-09-23 21:56:19 +0300

    .

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+function Header () {
+  return (
+    <div className="header">
+      Главная
+      Новости
+      Вход/Выход
+    </div>
+  );
+}
+
+export default Header;
